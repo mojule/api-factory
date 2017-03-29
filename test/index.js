c6f15613696492b8ab5927c2e2d1fe9a2f85adb7
Nik Coughlin <nrkn.com@gmail.com>
2017-03-30 11:39:04 +1300

    Working

--- test/index.js
+++ test/index.js
@@ -27,6 +27,9 @@ describe( 'ApiFactory', () => {
   const isPoint = point =>
     is.object( point ) && is.number( point.x ) && is.number( point.y )
 
+  const isIntegerPoint = point =>
+    is.object( point ) && is.integer( point.x ) && is.integer( point.y )
+
   const pointKey = point => `${ point.x } ${ point.y}`
 
   it( 'Creates an API', () => {
@@ -107,5 +110,140 @@ describe( 'ApiFactory', () => {
 
     assert.equal( i1.x(), 5 )
     assert.equal( i1.y(), 7 )
+  })
+
+  describe( 'private modules', () => {
+    const privateModule = ( api, state ) => {
+      return {
+        _stateKey: () => state.x + '-' + state.y
+      }
+    }
+
+    const publicModule = ( api, state ) => {
+      return {
+        stateKey: () => 'state-' + api._stateKey()
+      }
+    }
+
+    const overridePrivateModule = ( api, state ) => {
+      const { _stateKey } = api
+
+      return {
+        _stateKey: () => 'new-' + _stateKey()
+      }
+    }
+
+    it( 'modules can call private', () => {
+      const Point = ApiFactory( [ pointModule, privateModule, publicModule ] )
+
+      const p1 = Point({ x: 5.5, y: 7.5 })
+
+      const stateKey = p1.stateKey()
+
+      assert.equal( stateKey, 'state-5.5-7.5' )
+    })
+
+    it( 'removes private', () => {
+      const Point = ApiFactory( [ pointModule, privateModule, publicModule ] )
+
+      const p1 = Point({ x: 5.5, y: 7.5 })
+
+      assert.equal( typeof p1._stateKey, 'undefined' )
+    })
+
+    it( 'retains private', () => {
+      const Point = ApiFactory(
+        [ pointModule, privateModule, publicModule ],
+        { removePrivate: false }
+      )
+
+      const p1 = Point({ x: 5.5, y: 7.5 })
+
+      assert.equal( typeof p1._stateKey, 'function' )
+    })
+
+    it( 'overrides private', () => {
+      const Point = ApiFactory(
+        [ pointModule, privateModule, publicModule, overridePrivateModule ]
+      )
+
+      const p1 = Point({ x: 5.5, y: 7.5 })
+
+      const stateKey = p1.stateKey()
+
+      assert.equal( stateKey, 'state-new-5.5-7.5' )
+    })
+  })
+
+  describe( 'static modules', () => {
+    const staticModule = ( api, state ) => {
+      return {
+        $isPoint: isPoint,
+        isPoint: () => api.$isPoint( state )
+      }
+    }
+
+    const overrideStaticModule = ( api, state ) => {
+      return {
+        $isPoint: isIntegerPoint
+      }
+    }
+
+    it( 'api has statics', () => {
+      const Point = ApiFactory( [ pointModule, staticModule ] )
+
+      const p1 = { x: 5.5, y: 7.5 }
+
+      assert( Point.isPoint( p1 ) )
+      assert( !Point.isPoint( {} ) )
+    })
+
+    it( 'modules can call statics', () => {
+      const Point = ApiFactory( [ pointModule, staticModule ] )
+
+      const p1 = { x: 5.5, y: 7.5 }
+      const point1 = Point( p1 )
+
+      assert( point1.isPoint() )
+    })
+
+    it( 'removes statics', () => {
+      const Point = ApiFactory( [ pointModule, staticModule ] )
+
+      const p1 = { x: 5.5, y: 7.5 }
+      const point1 = Point( p1 )
+
+      assert.equal( typeof point1.$isPoint, 'undefined' )
+    })
+
+    it( 'retains statics', () => {
+      const Point = ApiFactory(
+        [ pointModule, staticModule ],
+        { removeStatic: false }
+      )
+
+      const p1 = { x: 5.5, y: 7.5 }
+      const point1 = Point( p1 )
+
+      assert.equal( typeof point1.$isPoint, 'function' )
+    })
+
+    it( 'overrides statics', () => {
+      const Point = ApiFactory(
+        [ pointModule, staticModule, overrideStaticModule ]
+      )
+
+      const p1 = { x: 5, y: 7 }
+      const p2 = { x: 5.5, y: 7.5 }
+
+      assert( Point.isPoint( p1 ) )
+      assert( !Point.isPoint( p2 ) )
+
+      const point1 = Point( p1 )
+      const point2 = Point( p2 )
+
+      assert( point1.isPoint() )
+      assert( !point2.isPoint() )
+    })
   })
 })
